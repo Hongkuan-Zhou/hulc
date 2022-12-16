27c15d4853197f73e748c04d3117266262180734
zhouh <ge49wej@tum.de>
2022-12-16 10:45:54 +0100

    enlarge language encoder network size

--- hulc/run_zhk.sh
+++ hulc/run_zhk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CUDA_VISIBLE_DEVICES=0,7  taskset -c 0-18 \
+CUDA_VISIBLE_DEVICES=1,2  taskset -c 0-18 \
 python training.py \
 trainer.gpus=-1 \
 datamodule.root_data_dir=dataset/task_D_D \
@@ -9,9 +9,8 @@ datamodule/datasets=vision_lang_shm \
 model/action_decoder=skill \
 model.action_decoder.perceptual_emb_slice.0=0 \
 model.action_decoder.out_features=18 \
-model.action_decoder.sg_chk_path='./checkpoints/SKILL_GENERATOR_2022-12-04_09-41-58' \
+model.action_decoder.sg_chk_path='./checkpoints/SKILL_GENERATOR_2022-12-09_21-34-22' \
 model/distribution=discrete \
 loss.kl_beta=0.0005 \
-loss.clip_auxiliary_loss_beta=0.01 \
-model.action_decoder.beta=0.01 \
-hydra.run.dir='../runs/2022-12-09/18-05-53'
+loss.clip_auxiliary_loss_beta=0.003 \
+model.action_decoder.beta=0.001
