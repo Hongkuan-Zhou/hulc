73e320e144305b71b25851d8d9603cf5260b4852
zhouh <ge49wej@tum.de>
2023-01-10 19:45:22 +0100

    updates

--- hulc/run_zhk.sh
+++ hulc/run_zhk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+HYDRA_FULL_ERROR=1 \
 CUDA_VISIBLE_DEVICES=1,2  taskset -c 0-18 \
 python training.py \
 trainer.gpus=-1 \
@@ -8,9 +9,10 @@ datamodule/datasets=vision_lang_shm \
 ~callbacks/tsne_plot \
 model/action_decoder=skill \
 model.action_decoder.perceptual_emb_slice.0=0 \
-model.action_decoder.out_features=18 \
-model.action_decoder.sg_chk_path='./checkpoints/SKILL_GENERATOR_2022-12-09_21-34-22' \
+model.action_decoder.skill_len=6 \
+model.action_decoder.out_features=20 \
+model.action_decoder.sg_chk_path='./checkpoints/SKILL_GENERATOR_2022-12-26_11-24-31' \
 model/distribution=discrete \
 loss.kl_beta=0.0005 \
-loss.clip_auxiliary_loss_beta=0.003 \
-model.action_decoder.beta=0.001
+loss.clip_auxiliary_loss_beta=0.01 \
+model.action_decoder.beta=0.005
